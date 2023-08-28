3feb84060a41d63d1961e326e6e7a0f30fe92b42
Malsha-Weerasinghe <weerasinghemalsha4@gmail.com>
2023-08-28 14:52:29 +0530

    create reservation controller

---
+++ hostel-management/src/lk/ijse/hostel/controller/ReservationController.java
@@ -0,0 +1,129 @@
+package lk.ijse.hostel.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import lk.ijse.hostel.dto.ReservationDTO;
+import lk.ijse.hostel.dto.RoomDTO;
+import lk.ijse.hostel.dto.StudentDTO;
+
+public class ReservationController {
+
+    @FXML
+    private TextField StuIdField;
+
+    @FXML
+    private RadioButton allBtn;
+
+    @FXML
+    private Button cancelBtn;
+
+    @FXML
+    private TableColumn<?, ?> colMoney;
+
+    @FXML
+    private TableColumn<?, ?> colPayStatus;
+
+    @FXML
+    private TableColumn<?, ?> colResDate;
+
+    @FXML
+    private TableColumn<?, ?> colResId;
+
+    @FXML
+    private TableColumn<?, ?> colRommType;
+
+    @FXML
+    private TableColumn<?, ?> colRoomDId;
+
+    @FXML
+    private TableColumn<?, ?> colRoomId;
+
+    @FXML
+    private TableColumn<?, ?> colRoomMoney;
+
+    @FXML
+    private TableColumn<?, ?> colRoomQty;
+
+    @FXML
+    private TableColumn<?, ?> colSId;
+
+    @FXML
+    private TableColumn<?, ?> colSName;
+
+    @FXML
+    private TableColumn<?, ?> colStuId;
+
+    @FXML
+    private TableColumn<?, ?> colStuName;
+
+    @FXML
+    private TableColumn<?, ?> colTypeRoomId;
+
+    @FXML
+    private Button deleteBtn;
+
+    @FXML
+    private Button editBtn;
+
+    @FXML
+    private Button newBtn;
+
+    @FXML
+    private RadioButton paidBtn;
+
+    @FXML
+    private RadioButton paidStatusBtn;
+
+    @FXML
+    private RadioButton pendingBtn;
+
+    @FXML
+    private RadioButton pendingStatusBtn;
+
+    @FXML
+    private DatePicker resDateField;
+
+    @FXML
+    private TextField resIdField;
+
+    @FXML
+    private Button reserveBtn;
+
+    @FXML
+    private TextField rooIdField;
+
+    @FXML
+    private TableView<ReservationDTO> tblReservation;
+
+    @FXML
+    private TableView<RoomDTO> tblRoom;
+
+    @FXML
+    private TableView<StudentDTO> tblStudent;
+
+    @FXML
+    void cancelBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void newBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reserveBtnOnAction(ActionEvent event) {
+
+    }
+}
