872ee51979d656a001413451e730f8804f0eaaa0
Malsha-Weerasinghe <weerasinghemalsha4@gmail.com>
2023-08-28 11:29:09 +0530

    set to loading dashboard

---
+++ hostel-management/src/lk/ijse/hostel/controller/DashBoardController.java
@@ -0,0 +1,49 @@
+package lk.ijse.hostel.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class DashBoardController {
+    @FXML
+    private ImageView backBtn;
+
+    @FXML
+    private Button reserveBtn;
+
+    @FXML
+    private Button roomBtn;
+
+    @FXML
+    private ImageView settingBtn;
+
+    @FXML
+    private Button studentBtn;
+
+    @FXML
+    void backBtnOnAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void reserveBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void roomBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void settingBtnOnAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void studentBtnOnAction(ActionEvent event) {
+
+    }
+}
