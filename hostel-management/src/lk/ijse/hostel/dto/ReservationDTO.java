604ee1087e3b62109f82b2f305394a2e58a9e1f8
Malsha-Weerasinghe <weerasinghemalsha4@gmail.com>
2023-08-28 15:38:51 +0530

    create reservationDTO

--- hostel-management/src/lk/ijse/hostel/dto/ReservationDTO.java
+++ hostel-management/src/lk/ijse/hostel/dto/ReservationDTO.java
@@ -1,4 +1,21 @@
 package lk.ijse.hostel.dto;
 
+import lk.ijse.hostel.entity.Room;
+import lk.ijse.hostel.entity.Student;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReservationDTO {
+    private String res_id;
+    private LocalDate res_date;
+    private String status;
+
+    private Student student;
+    private Room room;
 }
